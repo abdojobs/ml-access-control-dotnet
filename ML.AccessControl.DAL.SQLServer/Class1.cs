afa5100a0707138813057e9e94da0db2a2b49427
alafi <ahmadlafi@gmail.com>
2013-01-26 09:27:18 +0300

    Adding 2 Data Access Layer projects to solution for SQLite and SQL Server

---
+++ ML.AccessControl.DAL.SQLServer/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ML.AccessControl.DAL.SQLServer
+{
+    public class Class1
+    {
+    }
+}
---
+++ ML.AccessControl.DAL.SQLite/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ML.AccessControl.DAL.SQLite
+{
+    public class Class1
+    {
+    }
+}
