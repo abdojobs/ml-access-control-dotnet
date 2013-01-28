3d86b7909b1b074a54e994f6effa3a5e7e2a2170
alafi <ahmadlafi@gmail.com>
2013-01-28 15:57:48 +0300

    WinForms sample application added to demonstrate the capabilities of ML.AccessControl

---
+++ SampleWinForms/MainForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ML.AccessControl.BUS;
+
+namespace SampleWinForms
+{
+    public partial class MainForm : Form
+    {
+        BusManager _BLL = null;
+
+        public MainForm()
+        {
+            _BLL = new BusManager(
+               Type.GetType("ML.AccessControl.DAL.SQLite.DBManager, ML.AccessControl.DAL.SQLite", true),
+               "my connection string",
+               true);
+            InitializeComponent();
+        }
+    }
+}
